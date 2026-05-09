8e8bad9176cf91027de203960139743d3c254fca
Cristóbal Aguilera <cristobal.aguilera@sansano.usm.cl>
2026-05-09 15:08:08 -0400

    Se aumenta el número de iteraciones al generar la Strong Key para mayor seguridad

--- CODIGO/fase1.py
+++ CODIGO/fase1.py
@@ -61,7 +61,7 @@ def obtener_identidad(nombre, rol):
 
     # Tarea 1: Strong Key con PBKDF2
     passphrase = f"{nombre}{rol}".encode('utf-8')
-    strong_key = PBKDF2(passphrase, salt, dkLen=32, count=100000, hmac_hash_module=SHA256)
+    strong_key = PBKDF2(passphrase, salt, dkLen=32, count=600000, hmac_hash_module=SHA256)
     
     # Tarea 3: Generación Par de Llaves RSA-2048
     print(f"[*] Derivando llaves RSA de 2048 bits...")
